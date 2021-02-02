1757121e36109a0c562132a2fdd47ce2acc17e62
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-02 14:18:25 -0300

    feat(styles): Create theme file

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export default {
+    "colors": {
+        "primary": "#0d47a1",
+        "secondary": "#29b6f6",
+        "mainBg": "#171B35",
+        "contrastText": "#FFFFFF",
+        "wrong": "#dc3545",
+        "success": "#28a745"
+      },
+    "borderRadius": "4px"
+}
