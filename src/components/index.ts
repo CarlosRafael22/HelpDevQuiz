9968bf8366bf0dbbb1b886fd7ceef4c6a79e2504
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-03 14:45:09 -0300

    feat: Create index.ts on components folder

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import Widget from './Widget'
+// import QuizLogo from './QuizLogo'
+import QuizBackground from './QuizBackground'
+import QuizContainer from './QuizContainer'
+// import Footer from './Footer'
+import GitHubCorner from './GitHubCorner'
+import Question from './Question'
+
+export {
+    Widget,
+    QuizBackground,
+    QuizContainer,
+    GitHubCorner,
+    Question
+}
