b6ff385209b97436b9848f8ebd2e30634c16460a
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-03 12:41:32 -0300

    feat: Create QuizContainer component

---
+++ src/components/QuizContainer/index.tsx
@@ -0,0 +1,19 @@
+import styled, { css } from 'styled-components'
+
+type QuizContainerProps = {
+    showExpandedImage?: boolean
+}
+
+const QuizContainer = styled.div<QuizContainerProps>`
+  width: 100%;
+  max-width: 450px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    ${({ showExpandedImage }) => showExpandedImage && css`margin-top: 3rem;`}
+    padding: 15px;
+  }
+`
+
+export default QuizContainer
