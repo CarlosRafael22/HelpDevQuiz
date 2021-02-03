22c929b083c6f4c5302792828533ff0d5d35e90b
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-03 14:43:51 -0300

    feat: Add questions file on /utils

---
+++ src/utils/questions.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+        "image": "https://media.giphy.com/media/utio7lyz4Ehl6/giphy.gif",
+        "resultImage": "/ney2.jpg",
+        "title": "Em uma festa, Neymar chega com o que?",
+        "description": "Faz tempo em haha",
+        "answer": 3,
+        "alternatives": [
+            "Com a galeraaa",
+            "Com os salgadinhos",
+            "Com os copos plásticos",
+            "Com os refrii"
+        ]
+    },
+    {
+        "image": "https://media.giphy.com/media/26FeXfu01co671nck/giphy.gif",
+        "resultImage": "/ney3.jpg",
+        "title": "Agora, uma para salvar. Nas comemorações, é 100%...",
+        "description": "Faz tempo em haha",
+        "answer": 2,
+        "alternatives": [
+            "DEUS",
+            "EU",
+            "JESUS",
+            "NÓS"
+        ]
+    },
+]
+
+export default questions
